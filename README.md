ebc786e94a9580864a16f8ed768c8c7ea161784b
Sam-programs <None>
2023-10-15 18:19:19 +0300

    more fun haha

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ it forces a key to never be remaped
 ```lua
   require('mykey').setup('<tab>') -- never allow tab to be rempped by anything
 ```
+this plugin automatically maps return to the default mapping, making using autopair or completion plugins more fun :)
--- plugin/haha.lua
+++ plugin/haha.lua
@@ -1,16 +1,8 @@
-local   old_keymap_set = vim.keymap.set
-local   old_keymap_set_buf = vim.api.nvim_buf_set_keymap
-keymap_key = nil 
-keymap_mode = nil 
-keymap_function = nil
-keymap_opts = nil 
+local   _old_keymap_set = vim.keymap.set
+local   _old_keymap_set_buf = vim.api.nvim_buf_set_keymap
 
 local autocmd_handler = function(mode, mapkey, func, opts)
-   keymap_key = mapkey
-   keymap_mode = mode
-   keymap_function = func
-   keymap_opts = opts
-   vim.cmd('do <nomodeline> User keymap')
+   vim.cmd('do <nomodeline> User _keymap')
 end
 
 local autocmd_handler_buf = function(_,mode, mapkey, func, opts)
@@ -19,27 +11,18 @@ end
 
 local mykey = '<cr>'
 vim.api.nvim_create_autocmd({"User"}, {
-   pattern = "keymap",
+   pattern = "_keymap",
    callback = function()
-         old_keymap_set_buf(0,'i',mykey,mykey,{})
+         _old_keymap_set_buf(0,'i',mykey,mykey,{})
    end,
 })
 
 vim.keymap.set = function(...)
    autocmd_handler(...)
-   old_keymap_set(...)
+   _old_keymap_set(...)
 end
 
 vim.api.nvim_buf_set_keymap = function(...)
    autocmd_handler_buf(...)
-   old_keymap_set_buf(...)
+   _old_keymap_set_buf(...)
 end
-M = {}
-M.setup = function (key)
-   if type(key) ~= "string"  then
-      return
-   end
-   mykey = key
-end
-
-return M
